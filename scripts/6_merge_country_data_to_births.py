a145e89292f550ee015a671526896ad230f5fa48
krishnab <cyclotomiq@gmail.com>
2018-01-15 12:55:36 -0800

    Got the merge working now.

--- scripts/6_merge_country_data_to_births.py
+++ scripts/6_merge_country_data_to_births.py
@@ -7,7 +7,7 @@ def main_add_country_data(database):
     d.action_add_column_iso3_to_intersection_table()
     d.action_update_iso3_codes_for_country_data()
     d.get_country_column_names_and_add_to_intersection_table()
-    d.create_query_for_merging_country_data_into_intersection_table()
+    #d.create_query_for_merging_country_data_into_intersection_table()
     d.merge_country_data_into_intersection_table()
     print('completed addition of country data.')
 
--- scripts/test_scripts.py
+++ scripts/test_scripts.py
@@ -1,12 +1,13 @@
 
-from pydhs.Database import DatabasePsycopg2
-from pydhs.Database import DatabaseSqlalchemy
-from pydhs.Controller import Controller
+
 from pydhs.controller_countrydata import Controller_countrydata
 
 def main_add_country_data(database):
     d = Controller_countrydata(database)
-    d.create_query_for_merging_country_data_into_intersection_table()
+    #d.action_add_column_iso3_to_intersection_table()
+    #d.action_update_iso3_codes_for_country_data()
+    d.get_country_column_names_and_add_to_intersection_table()
+    print(d.create_query_for_merging_country_data_into_intersection_table())
     d.merge_country_data_into_intersection_table()
     print('completed addition of country data.')
 
